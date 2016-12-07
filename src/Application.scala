5433c930246982921cad9482fcc801420380d05e
Jared Bartrug <jerad4@gmail.com>
2016-12-07 16:58:57 -0500

    Finished up the save and load

--- src/Application.scala
+++ src/Application.scala
@@ -1,31 +1,97 @@
+import java.io.{File, PrintWriter}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.io.StdIn
+import scala.io.Source
+
 /**
   * Nick Trfz
   * Jared Bartrug
-  * Created by chrx on 12/6/16.
+  *
   */
 object Application {
   def main(args: Array[String]): Unit = {
-    load()
-    MainMenu()
+    val (students,teachers,classes) = load()
+    MainMenu(students,teachers,classes)
   }
 
-  def save(students: Array[Student], teachers: Array[Teacher], classes: Array[Classes]): Unit = {
+  def save(student: Array[Student], teacher: Array[Teacher], classes: Array[Classes]): Unit = {
+    val writerS = new PrintWriter(new File("Students.txt"))
+    val writerT = new PrintWriter(new File("Teachers.txt"))
+    val writerC = new PrintWriter(new File("Classes.txt"))
+    for (s <- student) {
+      val tempStr = s.name + " " + s.id.toString + " " + s.classes.mkString(",") + " " + s.grades.toList.mkString(", ") + "\n"
+      writerS.write(tempStr)
+    }
+    writerS.close()
+    for (c <- classes) {
+      val tempStr = c.name + " " + c.teacher + " " + c.students.mkString(",") + " " + c.grades.toList.mkString(", ") + "\n"
+      writerC.write(tempStr)
+    }
+    writerC.close()
+    for (t <- teacher) {
+      val tempStr = t.name + " " + t.classes.mkString(",") + "\n"
+      writerT.write(tempStr)
+    }
+    writerT.close()
 
   }
 
-  def load(): Unit = {
+  def load() = {
+    // Loading function loads all the students teachers and classes from file
+    //Opeing up the respective files
+    val studentInfo = Source.fromFile("Students.txt").getLines().toList
+    val students = ArrayBuffer[Student]()
 
+    val teacherInfo = Source.fromFile("Teachers.txt").getLines().toList
+    val teachers = ArrayBuffer[Teacher]()
+
+    val classInfo = Source.fromFile("Classes.txt").getLines().toList
+    val classes = ArrayBuffer[Classes]()
+
+    for (i <- studentInfo) {
+      val info = i.split(" ")
+      val values = info(3).split(", ")
+      val m = scala.collection.mutable.Map[String, Int]()
+      for (i <- values) {
+        val key = i.substring(1, i.length - 1).split(",")
+        m(key(0)) = key(1).toInt
+      }
+      students += new Student(info(0), info(1).toInt, info(2).split(","), m)
+    }
+
+    for (i <- teacherInfo) {
+      val info = i.split(" ")
+      val name = info(0)
+      val classes = info(1).split(",")
+      teachers +=  Teacher(name, classes)
+    }
+
+
+    for (i <- classInfo) {
+      val info = i.split(" ")
+      val values = info(3).split(", ")
+      val m = scala.collection.mutable.Map[String, Int]()
+      for (i <- values) {
+
+        val key = i.substring(1, i.length - 1).split(",")
+        m(key(0)) = key(1).toInt
+      }
+      classes += new Classes(info(0), info(1), info(2).split(","), m)
+    }
+    (students.toArray, teachers.toArray, classes.toArray)
   }
- def MainMenu(): Unit = {
+
+ def MainMenu(students:Array[Student],teachers:Array[Teacher],classes:Array[Classes]): Unit = {
    println("School Database")
    println("Please select a menu to enter\n1:Student\n2: Class\n3:Teacher")
-   val choice = readInt()
+   val choice = StdIn.readInt()
    choice match {
      case 1 => // Student
-       StudentMainMenu()
+       StudentMainMenu(students,teachers,classes)
 
      case 2 => //Class
-       ClassMainMenu()
+       ClassMainMenu(students,teachers,classes)
 
      case 3 => //Teacher
 
@@ -34,25 +100,25 @@ object Application {
        println("Invalid choice exiting")
    }
  }
-  def StudentMainMenu(): Unit = {
+  def StudentMainMenu(students:Array[Student],teachers:Array[Teacher],classes:Array[Classes]): Unit = {
     var firstChoice = 0
     var secondChoice = 0
     var invalidChoice = false
 
     while (!invalidChoice) {
-      println("\n1: Search Students by name\n2: Search students by ID\n3: See all Students")
-      firstChoice = readInt()
+      println("0: Back to main menu\n1: Search Students by name\n2: Search students by ID\n3: See all Students")
+      firstChoice = StdIn.readInt()
 
       firstChoice match {
         case 0 =>
           //back to main menu
-          MainMenu()
+          MainMenu(students,teachers,classes)
           sys.exit()
         case 1 =>
           //Search by name
           invalidChoice = false
           println("Search for a Name: ")
-          var name = readLine()
+          var name = StdIn.readLine()
 
         case 2 =>
         // Search by ID
@@ -77,11 +143,12 @@ object Application {
 
       if (secondChoice == 0) {
         //return to main menu
-        MainMenu()
+        MainMenu(students,teachers,classes)
         sys.exit()
       }
       else if (secondChoice == -1) {
         // exit program
+        save(students,teachers,classes)
         sys.exit()
       }
       else {
@@ -96,7 +163,7 @@ object Application {
     println("Classes: " + array(index).classes)
     println("Grades: " + array(index).grades)
   }
-  def ClassMainMenu(): Unit = {
+  def ClassMainMenu(students:Array[Student],teachers:Array[Teacher],classes:Array[Classes]): Unit = {
     println("\nList of Classes:")
     // print a list of all classes
   }
--- src/classes.scala
+++ src/classes.scala
@@ -1,16 +1,16 @@
 /**
   * Created by chrx on 12/6/16.
   */
-class Classes(teach:Teacher,coursename:String, student:Array[String], marks:Map[String,Int]){
+class Classes(teach:String,coursename:String, student:Array[String], marks:scala.collection.mutable.Map[String,Int]){
   val name = coursename
   val teacher = teach
   var grades = marks
   var students = student
 }
-class Student(fullname:String,ID:Int,schedule:Array[String], marks:Map[String,Int]){
+class Student(fullname:String,ID:Int,schedule:Array[String], marks:scala.collection.mutable.Map[String,Int]){
   val name = fullname
   val id = ID
   var classes = schedule
   var grades = marks
 }
-case class Teacher(name:String,classes:Array[Classes])
+case class Teacher(name:String,classes:Array[String])
